4bffb3ebfceabfb9e20e7a5d672b30764317a314
Liam <lkords@aol.com>
2025-08-02 19:42:14 -0700

    now it works

--- TreeQueries/typescript.scm
+++ TreeQueries/typescript.scm
@@ -1,7 +1,9 @@
+; Capture whole declarations
 (function_declaration) @chunk.function
 (class_declaration) @chunk.class
 (interface_declaration) @chunk.interface
 (enum_declaration) @chunk.enum
-; For extracting names:
-(function_declaration name: (identifier) @name)
-(class_declaration name: (identifier) @name)
+
+; Capture names separately
+(function_declaration name: (identifier) @function_name)
+(class_declaration name: (type_identifier) @name)
